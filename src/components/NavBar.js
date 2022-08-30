4c62a7a302020d84410b5a66a015aaac856302de
tucuvaldez <105434510+tucuvaldez@users.noreply.github.com>
2022-08-30 11:06:36 +0200

    modifico estilo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,21 +8,23 @@ function NavBar() {
         <Navbar bg="light" expand="lg">
             <Container>
                 <Navbar.Brand href="#home">Hygge Vinote</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Inicio</Nav.Link>
-                        <Nav.Link href="#link">Productos</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Vinos Blancos</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Vinos Tintos
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Otros</NavDropdown.Item>
+                <div>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Inicio</Nav.Link>
+                            <Nav.Link href="#link">Productos</Nav.Link>
+                            <NavDropdown title="Opciones" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Vinos Blancos</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Vinos Tintos
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Otros</NavDropdown.Item>
 
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
             </Container>
         </Navbar>
     );
